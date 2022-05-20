9fc35474199bed59a9a2b1e54b922573eedcfaca
Salman Khan <salmankhan@xmos.com>
2022-05-20 13:02:19 +0100

    add xcore target specification

---
+++ compiler/rustc_target/src/spec/xcore_unknown_unknown.rs
@@ -0,0 +1,25 @@
+use crate::spec::{LinkerFlavor, LldFlavor, PanicStrategy, RelocModel};
+use crate::spec::{Target, TargetOptions};
+
+pub fn target() -> Target {
+    Target {
+        data_layout: "e-m:e-p:32:32-i64:64-n32-S128".to_string(),
+        llvm_target: "xcore-unknown-unknown".to_string(),
+        pointer_width: 32,
+        arch: "xs2a".to_string(),
+
+        options: TargetOptions {
+            linker_flavor: LinkerFlavor::Lld(LldFlavor::Ld),
+            linker: Some("xmap".to_string()),
+            cpu: "xs2a-generic".to_string(),
+            max_atomic_width: Some(32),
+            features: "+m".to_string(),
+            executables: true,
+            panic_strategy: PanicStrategy::Abort,
+            relocation_model: RelocModel::Static,
+            emit_debug_gdb_scripts: false,
+            eh_frame_header: false,
+            ..Default::default()
+        },
+    }
+}
